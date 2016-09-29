d5961c832619f2a0c831ed1b8e8056a58ae55710
adarshjoy <adarshjoy@gmail.com>
2016-09-29 23:24:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/art-one', function (req, res) {
+  res.send ('test one' );
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
